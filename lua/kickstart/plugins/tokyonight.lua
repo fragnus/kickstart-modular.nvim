7b664af7e603b1b8c6c9f06525984237a3eee380
Magnus Lindbäck <magnus.lindback@epiroc.com>
2024-09-30 15:47:49 +0200

    Added cattpuccin and indent_line

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -10,12 +10,11 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 }
 -- vim: ts=2 sts=2 sw=2 et
