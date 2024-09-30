89005ea7e3aba618fe2b120bb0de4939cdada167
Magnus Lindbäck <magnus.lindback@epiroc.com>
2024-09-30 07:24:06 +0200

    Oil

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,13 @@
+-- File explorer that lets you edit your filesystem like a normal Neovim buffer
+-- https://github.com/stevearc/oil.nvim
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+}
